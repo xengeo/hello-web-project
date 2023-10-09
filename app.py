729315285ae716df5fd58dd97c20260d0a2ae692
xengeo <xen.geo4@gmail.com>
2023-10-09 17:17:21 +0100

    add app.y with new flask routes

--- app.py
+++ app.py
@@ -6,6 +6,84 @@ app = Flask(__name__)
 
 # == Your Routes Here ==
 
+
+# POST /sort-names
+#    Body Parameters:
+#       names=Joe,Alice,Zoe,Julia,Keiran  
+#    Expected response (200 OK):
+#    Alice,Joe,Julia,Kieran,Zoe
+@app.route('/sort-names', methods=['POST'])
+def sort_names():
+    if 'names' not in request.form:
+        return 'You didnt submit any names', 400
+    names = request.form['names']
+    if ',' not in names:
+        return 'Please provide a comma-separated list of names', 400
+    return ','.join(sorted(names.split(',')))
+
+
+
+
+# Request:
+# POST /count_vowels
+#   With body parameter: test=eee
+@app.route('/count_vowels', methods=['POST'])
+def count_vowels():
+    text = request.form['text']
+
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+
+    for letter in text:
+        if letter in vowels:
+            count += 1
+
+    return f'There are {count} vowels in "{text}"'
+
+
+# Request:
+# GET /wave
+#       With query parameter: name=Leo
+@app.route('/wave', methods=['GET'])
+def wave():
+    name = request.args['name']
+    return f'I am waving at {name}'
+
+
+# Request:
+# POST /submit
+#       With body parameters: name=Leo & message=Hello world
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name'] # Leo as a BODY PARAMTER hense request.form instead of request.args
+    message = request.form['message'] # Hello world
+
+    # send back a friendly message
+    return f'Thanks {name}, you send this message: "{message}"'
+
+
+# Request:
+# GET /hello?name=David
+#   With QUERY parameter: name=Leo
+@app.route('/hello', methods=['GET'])
+def hello():
+    name = request.args['name'] # The value is 'David'
+
+    # Send back a friendly greeting with the name
+    return f"Hello {name}!"
+
+
+# Request:
+# POST /goodbye
+#   With body parameter: name=Alice
+@app.route('/goodbye', methods=['POST'])
+def goodbye():
+    name = request.form['name'] # The value is 'Alice'
+
+    # Send back a fond farewell with the name
+    return f"Goodbye {name}!"
+
+
 # == Example Code Below ==
 
 # GET /emoji
